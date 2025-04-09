62dc6e9dbda289c3b4b45c29b653dc010a65310e
nomba38 <noe38concepts@gmail.com>
2025-04-09 17:36:47 -0400

    Updated vr controls

--- src/worlds/KrieghoffsEyes/assets/scripts/vr-controls.js
+++ src/worlds/KrieghoffsEyes/assets/scripts/vr-controls.js
@@ -1,17 +1,37 @@
 AFRAME.registerComponent('vr-controls', {
     init: function () {
         console.log("vr controls created.");
-        this.el.addEventListener('bPressed', this.bbuttondown.bind(this));
-        this.el.addEventListener('aPressed', this.abuttondown.bind(this));
+        this.el.addEventListener('thumbstickmoved', this.moveForwardBackward.bind(this));
     },
 
     //if thumbstick moved, move user
-    pressB: function (moveEvent) {
+    moveForwardBackward: function (moveEvent) {
+        //get the avatar rig
         const userRig = CIRCLES.getAvatarRigElement();
-    },
 
-    //if thumbstick moved, move user
-    pressA: function (moveEvent) {
-        const userRig = CIRCLES.getAvatarRigElement();
+        //get the camera of the rig
+        const userCam = rig.querySelector('.avatar');
+
+        //create vector showing user direction, and flatten/normalize it
+        const userDir = new THREE.Vector3();
+        userCam.object3D.getWorldDirection(userDirection);
+        userDir.y = 0;
+        userDir.normalize();
+
+        const userPos = userRig.object3D.position.clone();
+        const moveDist = 2;
+
+        if (moveEvent.detail.y < -0.95) { 
+            console.log("UP");
+            const forward = direction.clone().multiplyScalar(moveDist);
+            const newPos = userPos.add(forward);
+            rig.setAttribute('position', newPos);
+        }
+        if (moveEvent.detail.y > 0.95) { 
+            console.log("DOWN");
+            const backward = direction.clone().multiplyScalar(-1 * moveDist);
+            const newPos = userPos.add(backward);
+            rig.setAttribute('position', newPos);
+        }
     }
 })
