bc9053b87af1c86534f18a0a504c12e50afa0cd5
nomba38 <noe38concepts@gmail.com>
2025-04-09 14:35:59 -0400

    Update vr-controls

--- src/worlds/KrieghoffsEyes/assets/scripts/vr-controls.js
+++ src/worlds/KrieghoffsEyes/assets/scripts/vr-controls.js
@@ -1,29 +1,17 @@
 AFRAME.registerComponent('vr-controls', {
     init: function () {
         console.log("vr controls created.");
-        this.el.addEventListener('thumbstickmoved', this.moveThumbstick.bind(this));
+        this.el.addEventListener('bPressed', this.bbuttondown.bind(this));
+        this.el.addEventListener('aPressed', this.abuttondown.bind(this));
     },
 
     //if thumbstick moved, move user
-    moveThumbstick: function (moveEvent) {
+    pressB: function (moveEvent) {
         const userRig = CIRCLES.getAvatarRigElement();
-        const userCam = userRig.querySelector('[camera]');
-        const userDirection = new THREE.Vector3
-        userCam.object3D.getWorldDirection(userDirection);
-        userDirection.y = 0;
-        userDirection.normalize();
-        const currentPosition = userRig.object3D.position.clone();
-        if (moveEvent.detail.y < -0.95) { 
-            console.log("FORWARD");
-            userDirection.multiplyScalar(2);
-            const newPosition = currentPosition.add(userDirection);
-            userRig.setAttribute('position', newPosition); 
-        }
-        if (moveEvent.detail.y > 0.95) { 
-            console.log("BACKWARD");
-            userDirection.multiplyScalar(-2);
-            const newPosition = currentPosition.add(userDirection);
-            userRig.setAttribute('position', newPosition); 
-        }
+    },
+
+    //if thumbstick moved, move user
+    pressA: function (moveEvent) {
+        const userRig = CIRCLES.getAvatarRigElement();
     }
 })
