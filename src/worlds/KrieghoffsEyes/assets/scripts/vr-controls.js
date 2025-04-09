46e0136027c7d1733e1b32f698148ed682c71e18
nomba38 <noe38concepts@gmail.com>
2025-04-09 10:50:57 -0400

    controllers

--- src/worlds/KrieghoffsEyes/assets/scripts/vr-controls.js
+++ src/worlds/KrieghoffsEyes/assets/scripts/vr-controls.js
@@ -1,12 +1,22 @@
 AFRAME.registerComponent('vr-controls', {
     init: function () {
-        this.el.addEventListener('thumbstickmoved', this.moveThumbstick);
+        console.log("vr controls created.");
+        this.el.addEventListener('thumbstickmoved', this.moveThumbstick.bind(this));
     },
     //if thumbstick moved, move user
     moveThumbstick: function (moveEvent) {
-        if (moveEvent.detail.y > 0.95) { console.log("DOWN"); }
-        if (moveEvent.detail.y < -0.95) { console.log("UP"); }
-        if (moveEvent.detail.x < -0.95) { console.log("LEFT"); }
-        if (moveEvent.detail.x > 0.95) { console.log("RIGHT"); }
+        CIRCLES.getAvatarRigElement();
+        if (moveEvent.detail.y > 0.95) { 
+            console.log("DOWN");
+        }
+        if (moveEvent.detail.y < -0.95) { 
+            console.log("UP"); 
+        }
+        if (moveEvent.detail.x < -0.95) { 
+            console.log("LEFT"); 
+        }
+        if (moveEvent.detail.x > 0.95) { 
+            console.log("RIGHT"); 
+        }
     }
 })
